645b22e4a301ee4c90d4419a5884b41a5fb5feaa
Nahuewe <nahuelsoriap@gmail.com>
2023-11-10 02:13:00 -0300

    cambios en el calculo de precios

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ document.getElementById('btnAgregar').addEventListener('click', function () {
 
         mostrarDatosGuardados();
     } else {
-        alert('Por favor, complete todos los campos correctamente.');
+        alert('Completa los tres campos');
     }
 });
 
@@ -55,8 +55,11 @@ function mostrarDatosGuardados() {
     for (const nombre in datosGuardados) {
         const empanadas = datosGuardados[nombre];
         const tarjetaDiv = document.createElement('div');
-        tarjetaDiv.className = 'bg-white p-4 rounded-lg shadow-md';
-        tarjetaDiv.innerHTML = `<h2 class="text-xl font-bold mb-2">${nombre}</h2>`;
+        tarjetaDiv.className = 'bg-white p-4 rounded-lg shadow-md flex flex-col';
+
+        const tituloDiv = document.createElement('div');
+        tituloDiv.innerHTML = `<h2 class="text-xl font-bold mb-2">${nombre}</h2>`;
+        tarjetaDiv.appendChild(tituloDiv);
 
         empanadas.forEach(empanada => {
             const empanadaDiv = document.createElement('div');
@@ -68,15 +71,51 @@ function mostrarDatosGuardados() {
             tarjetaDiv.appendChild(empanadaDiv);
         });
 
+        const resultadoCalculoDiv = document.createElement('div');
+        resultadoCalculoDiv.className = 'mt-4 text-lg font-bold text-gray-700';
+        tarjetaDiv.appendChild(resultadoCalculoDiv);
+
+        const botonesDiv = document.createElement('div');
+        botonesDiv.className = 'flex items-center justify-between mt-2';
+
         const botonEliminar = document.createElement('button');
-        botonEliminar.className = 'bg-red-500 text-white px-2 py-1 rounded';
+        botonEliminar.className = 'bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 focus:outline-none ml-2';
         botonEliminar.innerText = 'Eliminar';
         botonEliminar.onclick = function () {
             eliminarTarjeta(nombre);
         };
+        botonesDiv.appendChild(botonEliminar);
 
-        tarjetaDiv.appendChild(botonEliminar);
+        const botonCalcular = document.createElement('button');
+        botonCalcular.className = 'bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none ml-2';
+        botonCalcular.innerText = 'Calcular';
+        botonCalcular.onclick = function () {
+            calcularPrecio(nombre, resultadoCalculoDiv);
+        };
+        botonesDiv.appendChild(botonCalcular);
+
+        tarjetaDiv.appendChild(botonesDiv);
         resultadoDiv.appendChild(tarjetaDiv);
+    }
+}
+
+function calcularPrecio(nombre, resultadoCalculoDiv) {
+    const precioInput = document.getElementById('precio');
+    const multiplicador = parseFloat(precioInput.value);
+
+    if (!isNaN(multiplicador) && multiplicador > 0) {
+        let cantidadTotal = 0;
+        const empanadas = datosGuardados[nombre];
+
+        empanadas.forEach(empanada => {
+            cantidadTotal += empanada.cantidad;
+        });
+
+        const cantidadMultiplicada = cantidadTotal * multiplicador;
+        resultadoCalculoDiv.innerText = `Total: $${cantidadMultiplicada}`;
+    } else {
+        resultadoCalculoDiv.innerText = 'Te falto el precio';
+        precioInput.focus();
     }
 }
 
